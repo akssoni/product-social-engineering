d6f9b1f5accdde9e8c5ebde153c49b76a1f4236b
akshay007soni@gmail.com <akssoni@users.noreply.github.com>
2014-05-23 04:32:41 +0000

    Product Page 22 May Evening

--- SourceCode/scripts/masttagJS.js
+++ SourceCode/scripts/masttagJS.js
@@ -55,7 +55,25 @@ $(function() {
     };
     
     
+    $( "#project-lightbox" ).autocomplete({
+      minLength: 0,
+      source: projects,
+      focus: function( event, ui ) {
+        $( "#project-lightbox" ).val( ui.item.label );
+        return false;
+      },
+      select: function( event, ui ) {
+        $( "#project-lightbox" ).val( ui.item.label );
+        return false;
+      }
+    })
+    .data( "ui-autocomplete" )._renderItem = function( ul, item ) {
+      return $( "<li>" )
+        .append( "<a><img src='img/threeiPhone.png' height='30' width='30' style='margin: 8px;'></img><div style='display: inline-block; vertical-align: top; margin:5px;'>" + item.label + "<br><span>" + item.desc + "</span></div></a>" )
+        .appendTo( ul );
+    };
     
+ 
     
     
     $(".page-master-header-account-info ul li").width($(".page-master-header-account-info").width()-2);
